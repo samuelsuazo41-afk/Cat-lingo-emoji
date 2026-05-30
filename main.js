bc3d5e9d367944219497b39c57a8d08a0617c569
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-30 07:58:04 +0200

    Update main.js

--- main.js
+++ main.js
@@ -101,6 +101,7 @@ function aplicarIdioma() {
   document.getElementById('tab-lectura-txt').textContent = LANG.tab_lectura;
   document.getElementById('tab-tips-txt').textContent = LANG.tab_tips;
   document.getElementById('tab-botiga-txt').textContent = LANG.tab_botiga;
+  document.getElementById('btn-lectura').textContent = LANG.lectura_btn;
 }
 
 function canviarTab(tab, e) {
